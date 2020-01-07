562ffd4b94fa75680c14b3568cde9d667f18d993
itsswill <willn2doit@gmail.com>
2020-01-06 22:34:56 -0500

    updated the square font style

--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
-  styleUrls: ['./square.component.scss']
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
